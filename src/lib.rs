e58ba1c3b7e60aefc0ef7dd77ae364f1fb19b318
MarcusTL12 <marcuspaafjellet@gmail.com>
2022-06-29 14:11:53 +0200

    Now it works

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,16 @@
+use libc::{c_double, c_int};
+
+extern "C" {
+    pub fn CINTgto_norm(l: c_int, e: c_double) -> c_double;
+}
+
 #[cfg(test)]
 mod tests {
+    use crate::*;
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn test_norm() {
+        let norm = unsafe { CINTgto_norm(1, 1.0) };
+
+        println!("Test1: {}", norm);
     }
 }
